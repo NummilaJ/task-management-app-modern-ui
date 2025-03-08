050d3ee1ef39e6df520d2e569608923fd95bf058
NummilaJ <jannenummila@gmail.com>
2025-03-08 21:23:01 +0200

    Korjattu Projektit-navigaatiokohdan käännös

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -85,7 +85,7 @@ import { ThemeService } from './services/theme.service';
                   <svg class="w-5 h-5 mr-3 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"></path>
                   </svg>
-                  Projektit
+                  {{ translate('projects') }}
                 </div>
               </a>
             </li>
